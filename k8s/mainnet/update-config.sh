4e49c3aca000e8a2274017fcb4eaf17c374d049c
Ludovit Scholtz <ludko2@gmail.com>
2024-02-22 09:00:37 +0100

    update cicd

--- k8s/mainnet/update-config.sh
+++ k8s/mainnet/update-config.sh
@@ -1,4 +1,4 @@
 kubectl apply -f deployment-main.yaml -n biatec
-kubectl delete configmap biatec-main-conf -n biatec
-kubectl create configmap biatec-main-conf --from-file=conf -n biatec
-kubectl rollout restart deployment/biatec-web-main-deployment -n biatec
+kubectl delete configmap biatec-dex-main-conf -n biatec
+kubectl create configmap biatec-dex-main-conf --from-file=conf -n biatec
+kubectl rollout restart deployment/biatec-dex-main-deployment -n biatec
