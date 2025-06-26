9ae79df2c4e32d2d0f14d307eb7947dc6db73472
Scholtz <ludko2@gmail.com>
2025-06-26 14:24:41 +0200

    cicd deploy

--- k8s/beta/update-config.sh
+++ k8s/beta/update-config.sh
@@ -2,3 +2,4 @@ kubectl apply -f deployment-beta.yaml -n biatec
 kubectl delete configmap biatec-dex-beta-conf -n biatec
 kubectl create configmap biatec-dex-beta-conf --from-file=conf -n biatec
 kubectl rollout restart deployment/biatec-dex-beta-deployment -n biatec
+kubectl rollout status deployment/biatec-dex-beta-deployment -n biatec
