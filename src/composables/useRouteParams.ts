d66359d38c1ce6a0fab4c1e85c426c5f23d3267f
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2025-10-24 18:15:35 +0200

    Implement useRouteParams composable for route parameter handling and refactor HomeView and ManageLiquidity components

---
+++ src/composables/useRouteParams.ts
@@ -0,0 +1,76 @@
+import { watch } from 'vue'
+import { useRoute } from 'vue-router'
+import { useAppStore } from '@/stores/app'
+import { AssetsService } from '@/service/AssetsService'
+
+export function useRouteParams() {
+  const store = useAppStore()
+  const route = useRoute()
+
+  const setRoutesVars = () => {
+    console.log('setRoutesVars', route.params)
+
+    if (route.params.network as 'mainnet-v1.0' | 'voimain-v1.0' | 'testnet-v1.0' | 'dockernet-v1') {
+      store.state.env = route.params.network as
+        | 'mainnet-v1.0'
+        | 'voimain-v1.0'
+        | 'testnet-v1.0'
+        | 'dockernet-v1'
+    }
+    if (route.params.assetCode) {
+      const code = route.params.assetCode as string
+      const asset = AssetsService.getAsset(code)
+      if (asset) {
+        store.state.assetCode = asset.code
+        store.state.assetName = asset.name
+
+        store.state.pair = AssetsService.selectPrimaryAsset(
+          store.state.currencyCode,
+          store.state.assetCode
+        )
+      }
+    }
+    if (route.params.currencyCode) {
+      const code = route.params.currencyCode as string
+      const asset = AssetsService.getAsset(code)
+      if (asset) {
+        store.state.currencyCode = asset.code
+        store.state.currencyName = asset.name
+        store.state.currencySymbol = asset.symbol
+
+        store.state.pair = AssetsService.selectPrimaryAsset(
+          store.state.currencyCode,
+          store.state.assetCode
+        )
+      }
+    }
+    console.log('store.state', store.state)
+  }
+
+  // Watch for route parameter changes
+  watch(
+    () => route.params.network,
+    () => {
+      setRoutesVars()
+    },
+    { deep: true }
+  )
+  watch(
+    () => route.params.assetCode,
+    () => {
+      setRoutesVars()
+    },
+    { deep: true }
+  )
+  watch(
+    () => route.params.currencyCode,
+    () => {
+      setRoutesVars()
+    },
+    { deep: true }
+  )
+
+  return {
+    setRoutesVars
+  }
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import Layout from '@/layouts/PublicLayout.vue'
-import { onMounted, watch } from 'vue'
+import { onMounted } from 'vue'
 
 import Message from 'primevue/message'
 import AsaChart from '@/components/TradingComponents/AsaChart.vue'
@@ -11,71 +11,11 @@ import TradesList from '@/components/LiquidityComponents/TradesList.vue'
 import { useAppStore } from '@/stores/app'
 import { useRoute } from 'vue-router'
 import { AssetsService } from '@/service/AssetsService'
+import { useRouteParams } from '@/composables/useRouteParams'
 
 const store = useAppStore()
 const route = useRoute()
-
-const setRoutesVars = () => {
-  console.log('setRoutesVars', route.params)
-
-  if (route.params.network as 'mainnet-v1.0' | 'voimain-v1.0' | 'testnet-v1.0' | 'dockernet-v1') {
-    store.state.env = route.params.network as
-      | 'mainnet-v1.0'
-      | 'voimain-v1.0'
-      | 'testnet-v1.0'
-      | 'dockernet-v1'
-  }
-  if (route.params.assetCode) {
-    const code = route.params.assetCode as string
-    const asset = AssetsService.getAsset(code)
-    if (asset) {
-      store.state.assetCode = asset.code
-      store.state.assetName = asset.name
-
-      store.state.pair = AssetsService.selectPrimaryAsset(
-        store.state.currencyCode,
-        store.state.assetCode
-      )
-    }
-  }
-  if (route.params.currencyCode) {
-    const code = route.params.currencyCode as string
-    const asset = AssetsService.getAsset(code)
-    if (asset) {
-      store.state.currencyCode = asset.code
-      store.state.currencyName = asset.name
-      store.state.currencySymbol = asset.symbol
-
-      store.state.pair = AssetsService.selectPrimaryAsset(
-        store.state.currencyCode,
-        store.state.assetCode
-      )
-    }
-  }
-  console.log('store.state', store.state)
-}
-
-watch(
-  () => route.params.network,
-  () => {
-    setRoutesVars()
-  },
-  { deep: true }
-)
-watch(
-  () => route.params.assetCode,
-  () => {
-    setRoutesVars()
-  },
-  { deep: true }
-)
-watch(
-  () => route.params.currencyCode,
-  () => {
-    setRoutesVars()
-  },
-  { deep: true }
-)
+const { setRoutesVars } = useRouteParams()
 
 onMounted(async () => {
   setRoutesVars()
